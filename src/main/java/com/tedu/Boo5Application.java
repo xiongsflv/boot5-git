6657dc25e03ff3c7c82ac2a181c78ac199574072
Le'novo <1610994178@qq.com>
2020-06-05 11:56:44 +0800

    首次提交项目boot5

---
+++ src/main/java/com/tedu/Boo5Application.java
@@ -0,0 +1,13 @@
+package com.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boo5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boo5Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/tedu/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.tedu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("info")
+    public String info(){
+        return "Hello Spring boot";
+    }
+}
